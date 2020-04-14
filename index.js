f32afeebf2303a2b613270720c30602ca9fc5e32
ten <suitok1@gmail.com>
2020-04-14 23:46:55 +0900

    send text

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const http = require('http').Server(app);
 const PORT = process.env.PORT || 7000;
 
 app.get('/',(req,res)=>{
-	//res.send('hello ずら');
-	res.sendFile(__dirname+'/index.html');
+	res.send('hello ずら');
+	//res.sendFile(__dirname+'/index.html');
 });
 http.listen(PORT,()=>{
 	console.log('server listening port:'+PORT);
