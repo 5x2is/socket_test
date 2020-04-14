99adfadcf711ab02d0cc22c6fe3aba07871d1ee5
ten <suitok1@gmail.com>
2020-04-15 00:26:25 +0900

    socket

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const PORT = process.env.PORT || 4000;
-
+const io = require('socket.io')(http);
 app.get('/',(req,res)=>{
 	//res.send('hello ずら');
 	res.sendFile(__dirname+'/index.html');
 });
-app.listen(PORT,()=>{
+io.on('connection',(socket)=>{
+	console.log('connected');
+});
+http.listen(PORT,()=>{
 	console.log('server listening port:'+PORT);
 });
