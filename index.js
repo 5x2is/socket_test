4dca86b2c28dfdcf38e850e0c184d36495117502
ten <suitok1@gmail.com>
2020-04-15 19:58:49 +0900

    mariadb2

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const io = require('socket.io')(http);
 const mariadb = require('mariadb');
 //const keys = JSON.parse(fs.readFileSync('../keys/keyList.json'));
 const keys = JSON.parse(process.env['MARIA']);
-mariadb.createConnection({
+const pool = mariadb.createPool({
 	host:keys.mariadb[0],
 	user:'root',
 	password:keys.mariadb[1],
 	port:3306
-}).then(conn=>{
-	conn.query('select 1',[2]).then(rows=>{
+});
+async function rowDatabase(queryText){
+	let conn;
+	try{
+		conn = await pool.getConnection();
+		const rows = await conn.query(queryText);
 		console.log(rows);
-		conn.end;
-	})
-}).catch(err=>{
-	console.log(err);
-	console.log('error');
-})
-/*
+	}catch(err){
+		console.log(err);
+	}finally{
+		if(conn){
+			return conn.end();
+		}
+	}
+}
 app.get('/',(req,res)=>{
 	res.sendFile(__dirname+'/index.html');
 });
 io.on('connection',(socket)=>{
 	socket.on('message',(msg)=>{
-		io.emit('message',msg);
+		rowDatabase('SHOW DATABASE');
+		//io.emit('message',msg);
 	});
 });
-var count = 0;
-function sendLog(){
-	io.emit('message',count);
-	count++;
-}
 
 http.listen(PORT,()=>{
 	console.log('server listening port:'+PORT);
 });
-*/
