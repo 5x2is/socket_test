9a9574c8360991f275e975259757be5379567bdc
ten <suitok1@gmail.com>
2020-04-15 19:40:22 +0900

    mariadb1

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const http = require('http').Server(app);
 const PORT = process.env.PORT || 4000;
 const io = require('socket.io')(http);
+const mariadb = require('mariadb');
+//const keys = JSON.parse(fs.readFileSync('../keys/keyList.json'));
+const keys = JSON.parse(process.env['MARIA']);
+mariadb.createConnection({
+	host:keys.mariadb[0],
+	user:'root',
+	password:keys.mariadb[1],
+	port:3306
+}).then(conn=>{
+	conn.query('select 1',[2]).then(rows=>{
+		console.log(rows);
+		conn.end;
+	})
+}).catch(err=>{
+	console.log(err);
+	console.log('error');
+})
+/*
 app.get('/',(req,res)=>{
-	//res.send('hello ずら');
 	res.sendFile(__dirname+'/index.html');
 });
 io.on('connection',(socket)=>{
 	socket.on('message',(msg)=>{
-//		console.log(msg);
 		io.emit('message',msg);
 	});
-//	setInterval(sendLog,2000);
 });
 var count = 0;
 function sendLog(){
@@ -23,3 +39,4 @@ function sendLog(){
 http.listen(PORT,()=>{
 	console.log('server listening port:'+PORT);
 });
+*/
